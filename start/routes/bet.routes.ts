6343256d7288aeb4b6580c5718d42de805c21deb
johnny <joaovlima07@hotmail.com>
2021-07-25 10:01:35 -0300

    feat: list by filter

--- start/routes/bet.routes.ts
+++ start/routes/bet.routes.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(()=> {
     Route.resource('bets', 'BetsController').apiOnly()
+    Route.get('filter', 'FilterBetsController.index')
 }).middleware(['auth'])
